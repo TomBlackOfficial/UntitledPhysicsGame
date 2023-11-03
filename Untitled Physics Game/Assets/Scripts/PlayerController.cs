bc052aaf025c33057a2ab8b5c5e35ba6eecac7ec
Tom Black <tomblackbusiness@gmail.com>
2023-11-03 15:56:46 -0700

    Added in stick man character and got driving, ragdoll not working

--- Untitled Physics Game/Assets/Scripts/PlayerController.cs
+++ Untitled Physics Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isPlayer1;
+
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float speed = 50;
+
+    private float movement;
+
+    private void Update()
     {
-        
+        if (isPlayer1)
+            movement = Input.GetAxis("Horizontal_P1");
+        else
+            movement = Input.GetAxis("Horizontal_P2");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
     }
 }
